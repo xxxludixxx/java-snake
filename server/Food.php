42560d1254dfce56835e3fde4d003537f4fbce0e
Jakub Ludwa <jludwa@creatuity.pl>
2017-06-27 15:16:58 +0200

    [INTCET-120] Developing server application - Food

---
+++ server/Food.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: jludwa
+ * Date: 27.06.17
+ * Time: 12:45
+ */
+class Food
+{
+    protected $foodX;
+    protected $foodY;
+
+    protected $randomPosition;
+
+    protected $snake;
+
+
+
+    public function createFood()
+    {
+
+
+
+    }
+
+    public function getFoodX()
+    {
+
+        return $this->foodX;
+
+    }
+
+    public function getFoodY()
+    {
+
+        return $this->foodY;
+
+    }
+
+
+
+}
